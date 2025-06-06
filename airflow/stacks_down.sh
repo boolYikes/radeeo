01e294303768b9eb5e762ac54dffbc39fa24a3af
Dongwon Henry Seon <tunacome@gmail.com>
2025-06-06 17:38:39 +0000

    fix: start script

--- airflow/stacks_down.sh
+++ airflow/stacks_down.sh
@@ -4,33 +4,31 @@ COMPOSE_CONFIG=/lab/dee/repos_side/radeeo/airflow/docker-compose.yaml
 echo "Listing all DAGs..."
 ALL_DAGS=$(docker compose -f "$COMPOSE_CONFIG" exec airflow-scheduler airflow dags list --output json | jq -r '.[].dag_id')
 
-# Pause all listed DAGs
+# Pause dags...
+echo "Pausing all running DAGs..."
 for DAG_ID in $ALL_DAGS; do
-    echo "Pausing DAG: $DAG_ID"
-    docker compose -f "$COMPOSE_CONFIG" exec airflow-scheduler airflow dags pause "$DAG_ID"
+    docker compose -f "$COMPOSE_CONFIG" exec airflow-scheduler airflow dags pause "$DAG_ID" > /dev/null 2>&1
 done
 
-# Wait for all DAGs to finish current runs
-echo "Checking for running dags before termination..."
+# ...and wait for runs to be done
 while true; do
-    ANY_RUNNING=false
-
+    running_tasks=0
     for DAG_ID in $ALL_DAGS; do
-        RUNNING_OUTPUT=$(docker compose -f "$COMPOSE_CONFIG" exec airflow-scheduler airflow dags list-runs -d "$DAG_ID" --state running)
-
-        if ! echo "$RUNNING_OUTPUT" | grep -q "No data found"; then
-            echo "DAG $DAG_ID still running..."
-            ANY_RUNNING=true
+        output=$(
+            docker compose -f "$COMPOSE_CONFIG" exec -T airflow-scheduler \
+            airflow dags list-runs -d "$DAG_ID" --state running
+        )
+        if [[ "$output" != *"No data found"* ]]; then
+            running_tasks=$((running_tasks + 1))
         fi
     done
 
-    if [ "$ANY_RUNNING" = false ]; then
-        echo "No running DAGs detected. Proceeding to terminate..."
+    if [ "$running_tasks" -eq 0 ]; then
+        echo "No running tasks left. Proceeding to terminate..."
         break
-    else
-        echo "Some DAGs still running... will check 5 sec later"
-        sleep 5
     fi
+    echo "$running_tasks task(s) still running... waiting 10s"
+    sleep 10
 done
 
 # Shut down Flower

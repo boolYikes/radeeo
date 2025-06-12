c04f886e2ad5d725da7867f38aceb197200ea275
Dongwon Henry Seon <tunacome@gmail.com>
2025-06-12 17:48:06 +0000

    feat: Termux alert added for failures

--- airflow/services/operators/local_sensor.py
+++ airflow/services/operators/local_sensor.py
@@ -1,13 +1,18 @@
+import os
+from airflow.exceptions import AirflowException
 from airflow.sensors.base import BaseSensorOperator
 from airflow.utils.decorators import apply_defaults
-import os
 
 class LocalFileSensor(BaseSensorOperator):
 
+    template_fields = ("filepath",)
+
     @apply_defaults
-    def __init__(self, filepath, **kwargs):
+    def __init__(self, filepath: str, **kwargs):
         super().__init__(**kwargs)
         self.filepath = filepath
 
     def poke(self, context):
-        return os.path.exists(self.filepath)
+        if os.path.exists(self.filepath):
+            return True
+        return AirflowException(f"File {self.filepath} not found")
